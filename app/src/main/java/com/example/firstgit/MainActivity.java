87376fcb2602951995f98a502dfa411c0782223d
JJH <wogh06@gmail.com>
2023-11-03 13:59:47 +0900

    First GitHub Commit

---
+++ app/src/main/java/com/example/firstgit/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.firstgit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("깃허브 연동");
+    }
+}
